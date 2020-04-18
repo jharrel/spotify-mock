75fc2689293628ba2b6fc788a2d67ffe1dcad396
jharrel <harrel.jason@gmail.com>
2020-04-18 13:30:22 -0700

    added music bar at the bottom and updated SCSS

--- src/App.js
+++ src/App.js
@@ -3,17 +3,20 @@ import './App.scss';
 
 function App() {
   return (
-    <div className="App">
-      <div className="navBar">
-        <div className="logo">
-          logo
+    <div className="outerWrap">
+      <div className="App">
+        <div className="navBar">
+          <div className="logo">
+            logo
+          </div>
+          <ul>
+            <li>Home</li>
+            <li>Search</li>
+            <li>Library</li>
+          </ul>
         </div>
-        <ul>
-          <li>Home</li>
-          <li>Search</li>
-          <li>Library</li>
-        </ul>
       </div>
+      <div className="musicControls">music controls</div>
     </div>
   );
 }
